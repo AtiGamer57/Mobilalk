3abaa80586e2f335d23ad6ee290b887ae4c3178d
Futó Attila <h157652@stud.u-szeged.hu>
2023-04-11 20:33:03 +0200

    Add login and registration form

--- SempeBolt/app/src/main/java/com/example/sempebolt/MainActivity.java
+++ SempeBolt/app/src/main/java/com/example/sempebolt/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.sempebolt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view) {
+        Log.i(TAG, "Login function got called");
+
+        EditText userName = findViewById(R.id.editTextUserName);
+        EditText password = findViewById(R.id.editTextPassword);
+
+    }
+
+    public void goToRegister(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
